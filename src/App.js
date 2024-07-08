7bf5d7be82e3a16939772b108acc90995f6b6c23
Jaymian-Lee <jaymian-lee@ziggo.nl>
2024-07-08 16:38:24 +0200

    chore(github): new github link & new contact styling

--- src/App.js
+++ src/App.js
@@ -85,9 +85,20 @@ const Portfolio = () => {
           height: 40px;
           fill: none;
           stroke: white;
-          stroke-width: 2;
+          stroke-width: 1;
           position: fixed;
           z-index: 2;
+        }
+        .icon2 {
+          width: 40px;
+          height: 40px;
+          fill: none;
+          position: fixed;
+          z-index: 2;
+        }
+        .github-icon {
+          left: 20px;
+          top: 20px;
         }
         .linkedin-icon {
           right: 20px;
@@ -101,38 +112,25 @@ const Portfolio = () => {
           position: fixed;
           top: 20px;
           right: 20px;
-          font-size: 16px;
+          background: none;
+          border: none;
+          cursor: pointer;
+          padding: 5px 10px;
+          font-size: 1.5vw;
           font-weight: 700;
+          text-align: center;
           font-family: 'Montserrat', sans-serif;
-          letter-spacing: 1px;
+          letter-spacing: 2px;
           color: transparent;
           -webkit-text-stroke: 1px rgba(255, 255, 255, 0.8);
           text-shadow: 
             0 0 10px rgba(255, 255, 255, 0.3),
             0 0 20px rgba(255, 255, 255, 0.2);
           transition: transform 0.1s ease;
-          background: none;
-          border: none;
-          cursor: pointer;
-          padding: 5px 10px;
-          background: rgba(255, 255, 255, 0.1);
-          border-radius: 5px;
-          border: 1px solid rgba(255, 255, 255, 0.8);
         }
-        @media (max-width: 600px) {
-          .stylized-name {
-            font-size: 12vw;
-          }
-          .slogan {
-            font-size: 4vw;
-          }
+        @media (max-width: 1268px) {
           .contact-button {
-            font-size: 12px;
-            padding: 3px 7px;
-          }
-          .icon {
-            width: 30px;
-            height: 30px;
+            font-size: 3vw;
           }
         }
       `}</style>
@@ -155,6 +153,12 @@ const Portfolio = () => {
         >
           Contact Me
         </button>
+        <a href="https://github.com/Jaymian-Lee" target="_blank" rel="noopener noreferrer">
+        <svg height="32" viewBox="0 0 16 16" className="icon2 github-icon" version="1.1" width="32" aria-hidden="true">
+  <path fill="#FFFFFF" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.58.82-2.14-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.03 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.14 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.003 8.003 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+</svg>
+
+        </a>
         <a href="https://www.linkedin.com/in/jaymian-lee-reinartz-9b02941b0/" target="_blank" rel="noopener noreferrer">
           <svg className="icon linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 10-4 0v7h-4v-7a6 6 0 016-6zM2 9h4v12H2V9zm2-7a2 2 0 110 4 2 2 0 010-4z"/>
